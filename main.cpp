92800ff7a649c5b280dd2abfb5c5b61e4dacdb2f
Stefan <admin@local.host>
2014-08-12 22:24:59 +0200

    Remove weird tabs

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@ int main()
 {
     std::cout << "Starting Engine" << std::endl;
 
+	
 	Engine engine; //= new Engine();
-		engine.build();
-		engine.run();
-		engine.stop();
+
+	engine.build();
+	engine.run();
+	engine.stop();
+
     return 0;
 }
 
