a50604845f4f1efea1c97ed29f8f6f07841ce8d7
Stefan <admin@local.host>
2014-10-29 22:29:55 +0100

    Remove shaders only cpu rendering

--- Engine.cpp
+++ Engine.cpp
@@ -6,13 +6,10 @@
 #include "Time.hpp"
 #include "Hud.hpp"
 #include "Renderer.hpp"
-#include "ShaderProgram.hpp"
 #include "Engine.hpp"
 #include "Scene.hpp"
 #include "Camera.hpp"
 #include "Light.hpp"
-#include "Mesh.hpp"
-#include "Texture.hpp"
 
 
 // Behaviorus
@@ -23,7 +20,15 @@
 #include "Colliders/SphereCollider.hpp"
 
 // Lights
+#include "Light.hpp"
+#include "Lights\PointLight.hpp"
 #include "Lights/Spotlight.hpp"
+
+// Primitives
+#include "Primitive.hpp"
+#include "Primitives\Sphere.hpp"
+#include "Primitives\Plane.hpp"
+
 
 
 
@@ -39,6 +44,7 @@ Engine::Engine() : hud(0), renderer(0), camera(0), light(0)
 	//renderer = new Renderer( window );
 	
 	//scene = new Scene("World");
+	Scene mScene("World");
 }
 
 Engine::~Engine()
@@ -48,6 +54,57 @@ Engine::~Engine()
 
 void Engine::build()
 {
+	// Create floor
+	//GameObject * floor = new GameObject( "Floor", glm::vec3( 0, 1, 0 ) );
+	//floor->setPrimitive( new Plane ( Material( Colour( 0.0f, 0.0f, 0.2f ), 1.0f, 1.0f, 0.2f ), Vector3( 0, 1, 0 ), 2.0f, false ) );
+	//mScene.add(floor);
+
+
+	//floor.setPrimitive(new Plane(Material(Colour(0.0f, 0.0f, 0.2f), 1.0f, 1.0f, 0.2f), Vector3(0, 1, 0), 2.0f, false));
+	//mScene.add(floor);
+	
+	std::shared_ptr<GameObject> floor(new GameObject("Test", glm::vec3(-8.0f, 3.0f, 8.0f)));
+	floor->setPrimitive(new Plane(Material(Colour(0.0f, 0.0f, 0.2f), 1.0f, 1.0f, 0.2f), Vector3(0, 1, 0), 2.0f, false));
+	mScene.add(*floor);
+
+
+	// Create spehere
+	GameObject * ball = new GameObject( "Ball", glm::vec3( -8.0f, 3.0f, 8.0f ) );	
+	ball->setPrimitive( new Sphere( Material( Colour( 1.0f, 3.0f, 0.1f), 0.2f, 0.0f, 1.0f ), Vector3( -8.0f, 3.0f, 8.0f ), 1.0f, true ) );
+	mScene.add(ball);
+
+	// Create Light.
+	GameObject * pointLight = new GameObject("pointLight", glm::vec3(5.0f, 25.0f, 0.0f));
+	pointLight->setLight( new PointLight(Colour(1.0f, 1.2f, 3.0f), Vector3(5.0f, 25.0f, 0.0f)));
+	mScene.add(pointLight);
+
+
+	// Top light
+	//mLights.push_back( new PointLight( Colour( 1.0f, 1.2f, 3.0f ), Vector3( 5.0f, 25.0f, 0.0f ) ) );
+
+
+	// Floor Plane
+	//mPrimitives.push_back(new Plane(Material(Colour(0.0f, 0.0f, 0.2f), 1.0f, 1.0f, 0.2f), Vector3(0, 1, 0), 2.0f, false));
+
+	// Blue sphere
+	//mPrimitives.push_back(new Sphere(Material(Colour(255.0f, 0.0f, 238.1f), 0.6f, 0.0f, 1.0f), Vector3(2.0f, 3.0f, 4.0f), 1.0f, true));
+	//mPrimitives.push_back(new Sphere(Material(Colour(1.0f, 3.0f, 0.1f), 0.2f, 0.0f, 1.0f), Vector3(-8.0f, 3.0f, 8.0f), 1.0f, true));
+	//mPrimitives.push_back(new Sphere(Material(Colour(0.0f, 0.0f, 0.1f), 0.2f, 1.0f, 1.0f), Vector3(-3.0f, 3.0f, 15.0f), 4.0f, true));
+	//mPrimitives.push_back(new Sphere(Material(Colour(1.0f, 1.0f, 0.1f), 0.2f, 0.0f, 1.0f), Vector3(6.0f, 3.0f, 1.0f), 1.0f, true));
+
+	// Mirror sphere
+	//	mPrimitives.push_back(new Sphere(Material(Colour( 0.0f, 0.0f, 1.0f ), 0.2f, 1.0f, 0.0f), Vector3(-7.5f, 2.0f, 5.0f), 3.0f, true));
+
+
+	//ball->setme
+
+	//std::unique_ptr<Primitive> ball(new Sphere(Material(Colour(1.0f, 3.0f, 0.1f), 0.2f, 0.0f, 1.0f), Vector3(-8.0f, 3.0f, 8.0f), 1.0f, true));
+	//scene->add
+	// Mirror sphere
+	//	mPrimitives.push_back(new Sphere(Material(Colour( 0.0f, 0.0f, 1.0f ), 0.2f, 1.0f, 0.0f), Vector3(-7.5f, 2.0f, 5.0f), 3.0f, true));
+
+
+
     // Shader List
 	//ShaderProgram * shaderPoint = new ShaderProgram("shaders/pointlight.vert", "shaders/pointLight.frag");
 
@@ -67,55 +124,28 @@ void Engine::build()
 	//scene.add(camera);
   //  scene->add( camera );
 
+	// Prepare Universe
+	mScene.prepare();
+
 }
 
-void Engine::run()
+bool Engine::run( Renderer* renderer )
 {
-	/*
-	running = true;
-	while ( running ) {
-		Time::update();
-		control();
-		if ( running ) { // control may have changed running;
-			update( Time::step() );
-			draw();
-		}
-	}*/
-
-	update(Time::step());
-	draw();
+	return draw( renderer );
 }
 
 void Engine::stop()
 {
-}
-
-// private functions
-// TODO Fix control
-void Engine::control()
-{
 
 }
-/*
-void Engine::control()
-{
-	sf::Event event;
-	while( window->pollEvent( event ) ) { // we must empty the event queue
-		if ( event.type == sf::Event::Closed ) {
-			window->close(); // oeps, still drawing after this one !!!!!!
-			running = false; // running = false;
-		}
-	}
-}*/
 
 
-void Engine::update( float step )
+bool Engine::draw( Renderer* renderer )
 {
-//	scene.update( step );
-}
 
-void Engine::draw()
-{
+	// Start Rendering
+	bool draw = renderer->draw( mScene );
+
 	//assert( window != 0 );
 	//assert( renderer != 0 );
 	//assert( scene != 0 );
@@ -125,4 +155,6 @@ void Engine::draw()
 
 	// TODO Fix window display renderer 
 	//window->display(); // swap colorbuffer to screen
+
+	return draw;
 }
